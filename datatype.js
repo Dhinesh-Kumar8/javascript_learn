e4e94b2cd091fbe9e327372f737dfb7af301c4ae
Dhinesh-Kumar8 <dhineshsurya@gmail.com>
2024-11-11 11:58:53 +0530

    hi2

---
+++ datatype.js
@@ -0,0 +1,17 @@
+/*There are seven primitive data types in JavaScript.
+These are string, number, Boolean, null, undefined, BigInt and symbol */
+/*Number*/
+let length = 12
+let weight = 45.2
+console.log(length)
+console.log(weight)
+/*string */
+let firstname = "john"
+let color = "red"
+console.log(firstname,color)
+/*boolean */
+var x1 = 100
+let x = true
+let y = false
+console.log(x,y)
+/* */
---
+++ operator.js
@@ -0,0 +1,24 @@
+let a =10
+let b = 20
+console.log(a+b)/*addition */
+console.log(a-b)/*subraction*/
+console.log(a*b)/*multipilcation */
+console.log(a/b)/*division */
+console.log(a>b)/*greater than*/
+console.log(a<b)/*lesser than*/
+console.log(a==b)/*equal to*/
+console.log(a!=b)/* not equal to*/
+var currentTime = 10
+console.log(currentTime > 9 && currentTime < 17)/*logical AND*/
+console.log(currentTime > 9 || currentTime < 17)/*logical OR*/
+var pet = true
+pet = !pet
+console.log(pet)/*logical NOT*/
+console.log(28 % 5)
+var mon = 1;
+var tue = 2;
+var wed = 1;
+var thu = 2;
+var fri = 3;
+console.log(mon + tue + wed + thu + fri); // 9
+
