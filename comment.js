43034f6d5e69de486603740a52e81ef8cee3a0b4
Dhinesh-Kumar8 <dhineshsurya@gmail.com>
2024-11-11 10:27:20 +0530

    hi

---
+++ comment.js
@@ -0,0 +1,11 @@
+//comment!
+/*Single line comment
+multi line comment*/
+/*single = "//"
+multi line ="/* "*/
+/*In JavaScript, the semi-colon - the ; character - has a similar purpose: it is used to clearly delimit parts of the code from some other parts of the code. */
+
+console.log("Hello World");
+/*Styling with %c: The use of %c in the first argument allows you to apply CSS styles to the text that follows. The second argument contains the CSS styles to be applied.
+Why It's Good*/
+console.log("%cHello, World", "color: blue; font-size: 40px");
