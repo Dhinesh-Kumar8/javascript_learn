ce5a28667daa95f202bb955732a0fd8ed0316b13
Dhinesh-Kumar8 <dhineshsurya@gmail.com>
2024-11-12 22:32:50 +0530

    2

--- object.js
+++ object.js
@@ -17,3 +17,47 @@ var assistantmanager ={
 }
 assistantmanager.nextachivement="get upgrading my self";
 console.log(assistantmanager);
+
+/*array are object
+push()->add of new item an array and
+pop()->remove the last from an array*/
+var fruit =[];
+fruit.push("apple");
+fruit.push("grape");
+fruit.push("orange");
+console.log(fruit);
+fruit.pop();
+console.log(fruit);
+
+function arr_bulid(id,name,number){
+    var arr =[];
+    arr.push(id);
+    arr.push(name);
+    arr.push(number);
+    console.log(arr);
+}
+arr_bulid(8,"dhinesh",9344898573);
+
+/*math object
+PI number->Math.PI
+Euler constant->Math.E
+natural logarithmof 2->Math.LN2*/
+console.log(Math.PI+" "+Math.E);
+
+/*Arthmetic and calculus*/
+console.log(Math.pow(2,3))/*power of*/
+console.log(Math.sqrt(25))/*square root*/
+console.log(Math.cbrt(8))/*cube root*/
+console.log(Math.max(2,10))
+console.log(Math.min(2,10))
+console.log(Math.sin(90))
+console.log("__________________")
+/*Math random */
+Math.random();
+var decimal = Math.random();
+console.log(decimal);
+console.log(decimal*10);
+
+/*Ceil method*/
+var round = Math.ceil(decimal);
+console.log(round);
